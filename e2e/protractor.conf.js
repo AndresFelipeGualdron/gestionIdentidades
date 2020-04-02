d6e910cb1c37989aea5c3d613b32ce6bfa5e1e16
AndresFelipeGualdron <gualdronsito@hotmail.com>
2020-04-02 11:04:04 -0500

    asdf

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://18.212.178.150:80/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
